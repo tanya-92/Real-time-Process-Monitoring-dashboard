efbec73847861c9ee74690015216c6caa8b6b76c
Tanya Yadav <tanyayadav19221@gmail.com>
2025-07-05 14:36:06 +0530

    Fix deployment: use eventlet and correct socketio run

--- app.py
+++ app.py
@@ -243,4 +243,5 @@ def monitor_disconnect():
     print('Client disconnected')
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
